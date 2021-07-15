9382b10cdaa4f40d1c0a955cae83a16580101946
Jesse Akoh <akohgodwinjesse@gmail.com>
2021-07-15 17:47:41 +0100

    ready for release

--- example/js/reUse.js
+++ example/js/reUse.js
@@ -7,7 +7,6 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
 var ReUseMl =
-/*#__PURE__*/
 function () {
   function ReUseMl() {
     _classCallCheck(this, ReUseMl);
