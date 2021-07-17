c314efbd41cfd97782f764edb3d8b12a5464c4c8
Akoh Jesse <akohgodwinjesse@gmail.com>
2021-07-17 14:31:04 -0700

    Update reUse.js

--- reUse.js
+++ reUse.js
@@ -1,13 +1,17 @@
-"use strict";
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) { 
+    throw new TypeError("Cannot call a class as a function");
+  } 
+}
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+function _defineProperties(target, props) { 
+  for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); }
+}
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
 var ReUseMl =
-/*#__PURE__*/
+    
 function () {
   function ReUseMl() {
     _classCallCheck(this, ReUseMl);
